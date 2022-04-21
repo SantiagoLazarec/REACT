9680825e01599f9841f2f15551ccf7398d691208
SantiagoLazarec <santiagolazarec014@gmail.com>
2022-04-20 23:12:54 -0300

    cart

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
-
+import CartW from './CartWidget'
 const NavBar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">HardComputers</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      
+  <div className="container-fluid">
+  <img src="https://ibb.co/BCJm942" className="navbar-brand" width="100px" alt="LogoHardComputers" />
+    <a className="navbar-brand" href="#">HardComputers</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Quienes somos</a>
-        <a class="nav-link" href="#">Productos</a>
-        <a class="nav-link" href="#">Como llegar</a>
-        <a class="nav-link disabled"></a>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-link active" aria-current="page" href="#">Quienes somos</a>
+        <a className="nav-link" href="#">Productos</a>
+        <a className="nav-link" href="#">Como llegar</a>
+        <a className="nav-link disabled"></a>
+        <CartW/>
       </div>
     </div>
   </div>
