c10dafd84aad9ad88e8c42454f797809b3e0dbb2
SantiagoLazarec <santiagolazarec014@gmail.com>
2022-05-10 20:06:15 -0300

    primera entrega

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import CartW from './CartWidget'
+
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       
   <div className="container-fluid">
   <img src="https://ibb.co/BCJm942" className="navbar-brand" width="100px" alt="LogoHardComputers" />
-    <a className="navbar-brand" href="/">HardComputers</a>
+    <Link  to="/" className="navbar-brand" href="/">HardComputers</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="/">Quienes somos</a>
-        <a className="nav-link" href="/">Productos</a>
-        <a className="nav-link" href="/">Como llegar</a>
-        <a className="nav-link disabled"></a>
+        <Link to="/QuienesSomos" className="nav-link active" aria-current="page" href="/">Quienes somos</Link>
+        <Link to="/Productos" className="nav-link" href="/">Productos</Link>
+        <Link to="/ComoLlegar" className="nav-link" href="/">Como llegar</Link>
+      
         <CartW/>
       </div>
     </div>
--- src/routes/rutas.jsx
+++ src/routes/rutas.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import QuienesSomos from '../components/QuienesSomos'
 
 const rutas = () => {
   return (
     <> 
     <BrowserRouter>
         <Routes>
-            
+          <Route index element={<Layout/>}> 
+            <Route index element={home}/>
+            <Route path="/QuienesSomos" element={QuienesSomos}/>
+            </Route>
         </Routes>
     
     </BrowserRouter>
